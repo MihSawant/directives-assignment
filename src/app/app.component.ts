255ccf9af929ddee72605321fefcdcd7b5cb5d44
Mihir Sawant <mih3190Sawant@outlook.com>
2022-06-24 23:35:45 +0530

    Used various angular built-in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .title-white{
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
   title = 'directives-app';
+  show = '';
+  count = 0;
+  counts = [];
+
+  onDisplayBtnClick(){
+    (this.count % 2 == 0) ? this.show = 'visible' : this.show = 'hidden';
+    this.count++;
+    this.counts.push(this.count);
+  }
+
+  hasCountHitFive(){
+    return this.counts.length >= 5;
+  }
+
+  
+  
 }
